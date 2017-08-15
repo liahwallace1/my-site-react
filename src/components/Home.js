f8a72808dd82060605af1e8c52a814006ad71df9
liahwallace1 <liahwallace1@gmail.com>
2017-08-15 16:26:27 -0400

    Formatted and wrote home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import './Home.css'
+import './Home.css';
+import photo from '../images/face_square.jpg';
 
 const Home = () =>
   <div className="home-container">
-  Home
+    <img className="home-me" src={photo} alt="Liah Wallace" />
+    <h3>HI! MY NAME IS</h3>
+    <h1>LIAH WALLACE</h1>
+    <hr width="40%"/>
+    <h4>Web Developer | Teacher | Student | Adventurer | Climber | Lover</h4>
   </div>
 
 export default Home
