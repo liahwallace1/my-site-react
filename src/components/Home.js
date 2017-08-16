1da0ed4b64682401c01c8aac490158b5db962a2f
liahwallace1 <liahwallace1@gmail.com>
2017-08-16 12:43:01 -0400

    added font awesome social media icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaGithubSquare, FaInstagram, FaLinkedinSquare, FaFacebookSquare, FaCommenting} from 'react-icons/lib/fa';
 import './Home.css';
 import photo from '../images/face_square.jpg';
 
@@ -9,6 +10,13 @@ const Home = () =>
     <h1>LIAH WALLACE</h1>
     <hr width="40%"/>
     <h4>Web Developer | Teacher | Student | Adventurer | Climber | Lover</h4>
+    <div className="social">
+      <a href="https://github.com/liahwallace1" target="_blank">  <FaGithubSquare /> </a>
+      <a href="https://www.linkedin.com/in/liah-wallace/" target="_blank"> <FaLinkedinSquare /> </a>
+      <a href="https://www.instagram.com/liahwalrus/" target="_blank"> <FaInstagram /> </a>
+      <a href="https://www.facebook.com/liah.baker.73" target="_blank"> <FaFacebookSquare /> </a>
+      <a href="http://liahdwallace.com/" target="_blank"> <FaCommenting /> </a>
+    </div>
   </div>
 
 export default Home
